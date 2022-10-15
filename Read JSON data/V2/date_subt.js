740cb4595e4dbe0ca90dbb2f8781f085896923bf
Vijender Kumar <75923641+Vijender-Kumar@users.noreply.github.com>
2022-10-15 15:25:54 +0530

    Add files via upload

--- Read JSON data/V2/date_subt.js
+++ Read JSON data/V2/date_subt.js
@@ -10,29 +10,36 @@ const cibilDateFormater = (date_account) => {
 
 let OpenedDate = new Date(cibilDateFormater(accounts[0].dateOpened));
 const formatYmd = date => date.toISOString().slice(0, 10);
-const ydate = formatYmd(OpenedDate);
-console.log("First Account opened on Date: " + ydate);
+const ldate = formatYmd(OpenedDate);
+console.log("Last Account opened on Date: " + ldate);
 
 const cdate = new Date();
 function getMonthDifference(startDate, endDate) {
     return (endDate.getMonth() - startDate.getMonth() + 12 * (endDate.getFullYear() - startDate.getFullYear()));
 }
-const vin_diff = getMonthDifference(new Date(ydate), new Date(cdate));
-console.log("Vintage in Months: " + vin_diff);
-
+const vin_diff = getMonthDifference(new Date(ldate), new Date(cdate));
+console.log("Vintage in Months for latest account: " + vin_diff);
 if(vin_diff > 24){
     console.log("Vintage difference is greater than 24 months !!!! Hence no dpd calculations for this account");
 }
 
-function subMonths(numOfMonths, date = new Date()) {
-    date.setMonth(date.getMonth() - numOfMonths);
-    return date;
-}
-const date = new Date();
-const strtDate = 24;
-for (let index = 0; index <= strtDate-1; index++) {
-    const ndate = subMonths(1, date);
-    const formatYmd = date => date.toISOString().slice(0, 10);
-    const ydate = formatYmd(ndate);
-    console.log(ydate);
-}
+let firstOpenedDate = new Date(cibilDateFormater(accounts[accounts.length -1].dateOpened));
+const formatfYmd = date => date.toISOString().slice(0, 10);
+const fdate = formatfYmd(firstOpenedDate);
+console.log("Last Account opened on Date: " + fdate);
+const vin_f_diff = getMonthDifference(new Date(fdate), new Date(cdate));
+console.log("Vintage in Months for first account: " + vin_f_diff);
+
+
+// function subMonths(numOfMonths, date = new Date()) {
+//     date.setMonth(date.getMonth() - numOfMonths);
+//     return date;
+// }
+// const date = new Date();
+// const strtDate = 24;
+// for (let index = 0; index <= strtDate-1; index++) {
+//     const ndate = subMonths(1, date);
+//     const formatYmd = date => date.toISOString().slice(0, 10);
+//     const ydate = formatYmd(ndate);
+//     console.log(ydate);
+// }
